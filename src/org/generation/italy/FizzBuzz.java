a8b0542299771d553c4cd67bb57b571257f5ca85
MatteoGenovese <matteo.genovese@icloud.com>
2022-11-15 17:27:59 +0100

    second commit

--- src/org/generation/italy/FizzBuzz.java
+++ src/org/generation/italy/FizzBuzz.java
@@ -1,5 +1,30 @@
 package org.generation.italy;
 
+import java.util.Iterator;
+
 public class FizzBuzz {
+	
+//	repo: java-fizzbuzz
+//	Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi 
+//	“Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che 
+//	sono sia multipli di 3 che di 5 stampi FizzBuzz.
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 100; i++) {
+			
+			if (i%3==0 && i%5!=0)
+				System.out.println("Fizz");
+			else if (i%3!=0 && i%5==0)
+				System.out.println("Buzz");
+			else if (i%3==0 && i%5==0)
+				System.out.println("FizzBuzz");
+			else
+			System.out.println(i);
+			
+		}
+		
+		
+	}
 
 }
